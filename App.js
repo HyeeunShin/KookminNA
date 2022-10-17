3187ebc8d402281182fb1633bda1a6543d093ee2
Baek-sohyeon <kitty224@kookmin.ac.kr>
2022-10-17 15:20:24 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,34 +1,11 @@
 import React from 'react';
+import {InformPage} from'./src/page';
 import {SearchPage, CalendarPage} from './src/page';
-import CalendarView from './src/component/CalendarView.js';
-import Config from 'react-native-config';
-import axios from 'axios';
-// const Stack = createStackNavigator();
-
-// console.log(Config.APP_API_KEY);
+import NameSearch from './src/component/Search/NameSearch.js';
 
 const App = () => {
-  // async function getData() {
-  //   const url = `https://open.assembly.go.kr/portal/openapi/TBPRESSCONF?KEY=${Config.APP_API_KEY}&pIndex=1&pSize=50&Type=json`;
-
-  //   await axios.get(url).then(response => {
-  //     const data = JSON.stringify(response.data);
-  //     console.log(data);
-  //   });
-  // }
-
-  // getData();
-
   return (
-    <>
-      {/* <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Search" component={SearchPage} />
-        </Stack.Navigator>
-      </NavigationContainer> */}
-      {/* <CalendarView /> */}
-      <SearchPage />
-    </>
+      <NameSearch/>
   );
 };
 
